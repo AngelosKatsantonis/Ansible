c840e7793f3d39bf8f62723ed9e9ed1c0a6ec1e3
aka <aggelos.katsantonis@hotmail.com>
2020-08-24 18:09:09 +0300

    Updates ansible local installation script to use python3

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Requires python,curl
-# On Debian sudo apt install python, curl
+# Requires python3,curl
+# On Debian sudo apt install python3, curl
 cd ~
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-pip install virtualenv
-git clone
-python -m virtualenv ansible
-source ansible/bin/activate
-pip install ansible
+python3 get-pip.py --user
+pip3 install virtualenv
+git clone https://github.com/AngelosKatsantonis/Ansible.git
+python3 -m virtualenv ansible_env
+source ansible_env/bin/activate
+pip3 install ansible,boto3,boto
+mkdir -p /etc/ansible
+cp -R ansible/* /etc/ansible
