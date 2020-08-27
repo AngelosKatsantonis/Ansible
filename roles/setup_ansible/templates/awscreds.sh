082848485747619069ae82a2e5a7b9f53497b9da
aka <aggelos.katsantonis@hotmail.com>
2020-08-27 04:11:02 +0300

    test

--- roles/setup_ansible/templates/awscreds.sh
+++ roles/setup_ansible/templates/awscreds.sh
@@ -1,6 +1,6 @@
-export EC2_INI_PATH=/etc/ansible/ec2.ini
-export ANSIBLE_INVENTORY=/etc/ansible/hosts
+export EC2_INI_PATH=~/ansible/ec2.ini
+export ANSIBLE_INVENTORY=~/ansible/hosts
+export ANSIBLE_CONFIG=~/ansible/ansible.cfg
 export AWS_ACCESS_KEY_ID=<aws access key id>
 export AWS_SECRET_ACCESS_KEY=<aws access key>
 export AWS_REGION=eu-west-1
-# Can add more export to setup a playbook execution environment
